5e8103cdaf85d8821a56597d563eeddc41f66887
Michael Coleman <michaelcoleman135@gmail.com>
2018-11-14 21:31:39 +0000

    Added upload, convert and display image
    added the ability to upload an image, then have it converted to binnary
    64 and read from mongodb and displayed on the page

--- src/app/create-car-ad/create-car-ad.component.ts
+++ src/app/create-car-ad/create-car-ad.component.ts
@@ -9,13 +9,33 @@ import {PostadvertService} from '../services/postadvert.service';
   styleUrls: ['./create-car-ad.component.css']
 })
 export class CreateCarAdComponent implements OnInit {
+  photoBinaryString:string="";
+  // adapted from https://stackoverflow.com/questions/42482951/converting-an-image-to-base64-in-angular-2
+  handleFileSelect(evt){
+    var files = evt.target.files;
+    var file = files[0];
+  
+  if (files && file) {
+      var reader = new FileReader();
+
+      reader.onload =this.handleReaderLoaded.bind(this);
+
+      reader.readAsBinaryString(file);
+  }
+}
+
+handleReaderLoaded(readerEvt) {
+   var binaryString = readerEvt.target.result;
+          //converts string to base 64
+          this.photoBinaryString= btoa(binaryString);
+          console.log(this.photoBinaryString);
+  }
 
   constructor(private service:PostadvertService) { }
 
   onAddPost(form: NgForm) {
-
     this.service.addPost(form.value.name, form.value.password,form.value.phone,form.value.email, form.value.make, form.value.model, form.value.year, form.value.price, 
-      form.value.colour, form.value.fuel, form.value.photo).subscribe();
+      form.value.colour, form.value.fuel, this.photoBinaryString).subscribe();
     
     console.log(form.value);
     form.resetForm();
