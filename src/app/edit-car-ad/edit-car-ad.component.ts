a4c73bf949310a3f3bf8df0b4dd923fee8e69a6a
Michael Coleman <michaelcoleman135@gmail.com>
2018-11-16 21:38:10 +0000

    Commented Some code
    commented the convert to base 64 image code

--- src/app/edit-car-ad/edit-car-ad.component.ts
+++ src/app/edit-car-ad/edit-car-ad.component.ts
@@ -10,28 +10,29 @@ import {PostadvertService} from '../services/postadvert.service';
   styleUrls: ['./edit-car-ad.component.css']
 })
 export class EditCarAdComponent implements OnInit {
-  carPost: any = [];
-  photoBinaryString:string="";
-  // adapted from https://stackoverflow.com/questions/42482951/converting-an-image-to-base64-in-angular-2
-  handleFileSelect(evt){
-    var files = evt.target.files;
-    var file = files[0];
+    carPost: any = [];
+    photoBinaryString:string="";
+    //convert photo into base64
+    // adapted from https://stackoverflow.com/questions/42482951/converting-an-image-to-base64-in-angular-2
+    handleFileSelect(evt){
+        var files = evt.target.files;//can be found in file(evt) array returned by selecting file, can viewed in console by logging evt
+        var file = files[0];
+      
+      if (files && file) {
+          var reader = new FileReader();
+
+          reader.onload =this.handleReaderLoaded.bind(this);//call below method handleReaderLoaded
+
+          reader.readAsBinaryString(file);
+      }
+    }
+
+  handleReaderLoaded(readerEvt) {
+    var binaryString = readerEvt.target.result;
+            //btoa() converts string to base 64
+            this.photoBinaryString= btoa(binaryString);
+    }
   
-  if (files && file) {
-      var reader = new FileReader();
-
-      reader.onload =this.handleReaderLoaded.bind(this);
-
-      reader.readAsBinaryString(file);
-  }
-}
-
-handleReaderLoaded(readerEvt) {
-   var binaryString = readerEvt.target.result;
-          //converts string to base 64
-          this.photoBinaryString= btoa(binaryString);
-  }
-
   constructor(private route: ActivatedRoute,private service:PostadvertService,private router:Router) { 
     this.router.routeReuseStrategy.shouldReuseRoute = function() {
       return false;
