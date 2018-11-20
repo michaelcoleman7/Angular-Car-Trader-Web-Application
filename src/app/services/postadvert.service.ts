7c12ebe5e7311bd81448557b7f39c926c55af3fc
Michael Coleman <michaelcoleman135@gmail.com>
2018-11-20 21:29:13 +0000

    commented service and updated too;bar colour

--- src/app/services/postadvert.service.ts
+++ src/app/services/postadvert.service.ts
@@ -8,7 +8,7 @@ import {Car} from '../car.model';
 })
 export class PostadvertService {
   constructor(private http: HttpClient) { }
-  
+    //get all car posts from mongoDB via server
     getPostsData(): Observable<any> {
       return this.http.get("http://localhost:8081/getallcars");
     }
@@ -18,7 +18,7 @@ export class PostadvertService {
   getPosts() {
     return [...this.posts];
   }
-
+  //add post to mongoDB via server
   addPost(name: string, password:string,phone: number,
     email: string,make: string, model: string, year: number, price: number, 
      colour: string, fuel: string,photo: string): Observable<any> {
@@ -27,14 +27,15 @@ export class PostadvertService {
     return this.http.post("http://localhost:8081/postcar",post);
   }
 
+  //delete post from mongoDB via server
   deletePost(id: string):Observable<any>{
     return this.http.delete("http://localhost:8081/deletecar/"+id);
   }
-
+  //get post by id from mongoDB via server
   getPost(id: string):Observable<any>{
     return this.http.get("http://localhost:8081/getcars/"+id);
   }
-
+  //Update car ad with new information to mongoDB via server
   updateCar(id:String, name: string, password:string,phone: number,email: string,make: string, model: string, year: number, price: number, 
      colour: string, fuel: string,photo: string): Observable<any>{
     const post: Car = {name: name, password:password,phone:phone, email: email, make: make,
